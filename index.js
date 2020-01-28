df09cae9b38798043484eb6cd9abf7b71d6a3cd6
Anas K <mail.anascape@gmail.com>
2020-01-28 10:37:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
+  const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
+
+  var arr = []
+
+  for(i = 0; i < 4; i++ ){
+var counter = `${musicians[]} plays ${instruments[]}`
+
+arr.push(counter)
+
+  }
+}
